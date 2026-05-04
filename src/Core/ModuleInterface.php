92456adf4c2725f8d738b71d51da0f4067c896da
AlAtaKe (Luis Enrique GA) <lenriquega@gmail.com>
2026-05-04 13:09:01 -0600

    Se integra versión actual de Code Showcase para refactor

--- src/Core/ModuleInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Triskelion\TriskelionToolkit\Core;
-
-interface ModuleInterface {
-	public static function get_module_id(): string;
-	public static function get_module_args(): array;
-}
